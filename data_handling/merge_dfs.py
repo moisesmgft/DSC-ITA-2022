dde6bcbd2dbd1cf8a185976c463e0126382e0fb6
moisesmgft <87101637+moisesmgft@users.noreply.github.com>
2022-10-18 16:31:13 -0300

    data handling utilities

--- data_handling/merge_dfs.py
+++ data_handling/merge_dfs.py
@@ -13,9 +13,9 @@ for i in range(1,len(files)):
     df2 = pd.read_csv(files[i])
     df2 = df2.rename({'Date': 'Dia','Close': files[i].replace(src_path,'').replace('.csv','')}, axis=1)
     #df1.merge(df2, how='left', left_on='Date', right_on='Date')
-    df1 = pd.merge(df1, df2, how='outer', on=['Dia','Dia']) # talvez inner, pq faz a interseccao
+    df1 = pd.merge(df1, df2, how='inner', on=['Dia','Dia']) # talvez inner, pq faz a interseccao
 
 
 df1['Dia'] = pd.to_datetime(df1['Dia'])
 df1 = df1.sort_values(by='Dia')
-df1.to_csv('data_set.csv')
+df1.to_csv('dataset_inner.csv')
